954c535f2c8aff625336c399e55a106c427a7d6b
maftuhihsan162 <maftuhihsan1602@gmail.com>
2020-11-25 19:20:32 +0700

    Adding Wisata activity

---
+++ app/src/main/java/com/example/slurteam/api/Api.java
@@ -0,0 +1,9 @@
+package com.example.slurteam.api;
+
+public class Api {
+
+    public static String Wisata = "https://dev.farizdotid.com/api/purwakarta/wisata";
+    public static String DetailWisata = "https://dev.farizdotid.com/api/purwakarta/wisata/{id}";
+
+
+}
