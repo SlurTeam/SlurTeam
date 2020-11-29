e962b7abaf2a6b1e9b3e51b9fa93c2afca420f65
Thomas2213 <thomasalkadafi@gmail.com>
2020-11-29 15:31:56 +0700

    alarm

---
+++ app/src/main/java/com/example/slurteam/activities/AlarmReceiver.java
@@ -0,0 +1,56 @@
+package com.example.slurteam.activities;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.slurteam.R;
+
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mNotificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        deliverNotification(context);
+    }
+
+    private void deliverNotification(Context context) {
+
+        Intent contentIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent
+                        .FLAG_UPDATE_CURRENT);
+        // Build the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notif)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_text))
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        // Deliver the notification
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
